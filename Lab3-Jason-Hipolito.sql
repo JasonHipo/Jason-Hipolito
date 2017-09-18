5b173b74502435567bcee6fb921b629a96f76c25
JasonHipo <31450705+JasonHipo@users.noreply.github.com>
2017-09-17 22:55:45 -0400

    Add files via upload
    This is Lab 3 where i show a few Queries using the CAP database.

---
+++ Lab3-Jason-Hipolito.sql
@@ -0,0 +1,50 @@
+--Jason Hipolito
+--Lab 3
+
+--Query 1
+Select ordno, totalusd
+from orders;
+
+--Query 2
+Select name, city
+from agents
+where name = 'Smith';
+
+--Query 3
+Select pid, name, priceusd
+from products
+where qty > 200010;
+
+--Query 4
+Select name, city
+From customers
+where city = 'Duluth';
+
+--Query 5 
+Select name, city
+from agents
+where city != 'New York' and city != 'Duluth';
+
+--Query 6 (check)
+Select *
+From products
+Where (city != 'Dallas' 
+and city != 'Duluth' )
+and priceusd > 1;
+
+--Query 7
+Select *
+From orders
+Where month = 'Mar'
+or month = 'May';
+
+--Query 8 
+Select * 
+From orders
+Where month = 'Feb'
+and totalusd >= 500;
+
+--Query 9
+Select * 
+From orders
+Where cid = 'c005';
