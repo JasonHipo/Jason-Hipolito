dd6583874188990143d1bac42e081e5d11d2728e
JasonHipo <31450705+JasonHipo@users.noreply.github.com>
2017-09-24 21:59:21 -0400

    Database lab 4 (subqueries)
    This lab shows a few different ways to write subqueries with multiple tables without using any join commands.

--- Jason Hipolito DB_HW4.sql
+++ Jason Hipolito DB_HW4.sql
@@ -5,33 +5,71 @@ where aid in(Select aid
             From orders
             where cid = 'c006');
                         
---Query 2 (try to compress)
-Select distinct pid 
-from products 
-where pid in (select pid
-             from orders
-             where aid in(select aid
-                         from orders
-                         where cid in(select cid
-                                     from customers 
-                                     where city = 'Beijing')
-                         )
-             )
+--Query 2 
+select distinct pid
+from orders
+where aid in(select aid
+                from orders
+                where cid in(select cid
+                                from customers
+                                where city = 'Beijing'))
 order by pid asc;
 
 --Query 3 (check)
-Select cid, name 
-from customers 
-where cid in(select cid
-            from orders
-            where aid != 'a03');
-            
---Query 4 (should return c001, c006 but its also giveing me c004)
-Select cid 
+
+ select distinct cid
+ from customers
+ where cid not in(select cid
+                     from orders
+                     where aid = 'a03');
+--Query 4
+Select distinct cid
+from orders
+where pid = 'p01'
+and cid in(select cid
+           from orders
+           where pid = 'p07');
+--Query 5
+select pid
+from products
+where pid not in
+                (select pid
+                from orders
+                where aid = 'a02'
+                or aid = 'a03')
+order by pid desc;
+
+--Query 6
+select name, discountpct, city
 from customers
-where cid in (select cid
-             from orders
-             where pid = 'p01'
-             or pid in (select pid
-                        from orders 
-                        where pid = 'p07'));
+where cid in
+            (select cid
+            from orders
+            where aid in
+                        (Select aid
+                        from agents
+                        where city = 'Tokyo'
+                        or city = 'New York'));
+                        
+                        
+--Query 7
+select * 
+from customers
+where discountpct in(
+                    select discountpct
+                    from customers
+                    where city = 'Duluth'
+                    or city = 'London');
+                    
+--Question 8
+--a check constraint is something you put in your database to avoid entering 
+--bad database that is not relavent to that column. the advantage of doing this
+--would be so that no wrong data can be entered and it will help keep the integity
+--of the data. for example if you want to enter a month then you would want to
+--add a check constraint that makes sure you are entering something like: 09, sept, or september.
+--depending on what the standard for the DB is. a bad check would let you imput something
+--like: hotdog, 13, ect in the month column. this would be bad because if someone
+--else later tries to find out the date of what ever data they are looking for it
+--would be meaningless data. 
+                    
+                    
